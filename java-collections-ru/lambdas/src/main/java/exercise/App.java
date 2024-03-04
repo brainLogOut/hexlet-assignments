36b14748fee0091675e8f53a8b285961538ade23
@hexlet/cli <support@hexlet.io>
2024-03-04 21:58:36 +0500

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -1,47 +1,23 @@
 package exercise;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 // BEGIN
 public class App {
-    public static void main(String[] args) {
-        String[][] image = {
-                {"*", "*", "*", "*"},
-                {"*", " ", " ", "*"},
-                {"*", " ", " ", "*"},
-                {"*", "*", "*", "*"},
-        };
 
-        String[][] editedMassive = Arrays.stream(image)
-                .map(m -> {
-                    int newLength = m.length * 2;
-                    String[] x2Massive = new String[newLength];
+    public static String[][] enlargeArrayImage(String[][] image) {
 
-                    for (int i = 0; i < m.length; i++) {
-                        x2Massive[i + (m.length / 2)] = m[i];
-                    }
-
-                    return x2Massive;
-                })
+        return Arrays.stream(image)
+                .map(App::duplicateValues)
+                .flatMap(m -> Arrays.stream(new String[][] {m, m}))
                 .toArray(String[][]::new);
+    }
 
-        for (String[] strings : editedMassive) {
-            System.out.println(Arrays.toString(strings));
-        }
+    public static String[] duplicateValues(String[] items) {
 
-//        List<List<String>> ret1 = Arrays.stream(image)
-//                        .map(arr -> Arrays.asList(arr))
-//                                .toList();
-
-        //System.out.println(back);
-
-//        List<List<String>> imageInstring = Arrays.stream(Arrays.stream(image)
-////                .flatMap(m -> Arrays.stream(m))
-//                .map(m -> Arrays.asList(m));
+        return Arrays.stream(items)
+                .flatMap(item -> Arrays.stream(new String[] {item, item}))
+                .toArray(String[]::new);
     }
 }
 // END
--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -1,8 +1,37 @@
 package exercise;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 // BEGIN
+class AppTest {
 
+    @Test
+    void takeTest() {
+        String[][] image = {
+                {"*", "*", "*", "*"},
+                {"*", " ", " ", "*"},
+                {"*", " ", " ", "*"},
+                {"*", "*", "*", "*"},
+        };
+
+        String[][] enlargeImage = {
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                {"*", "*", "*", "*", "*", "*", "*", "*"},
+        };
+
+        assertThat(App.enlargeArrayImage(image)).isDeepEqualTo(enlargeImage);
+    }
+
+}
 // END
